5378c57073858db7c8e4e762cd00fa573685ebc6
admin <admin@DESKTOP-4RM8KQM>
2025-04-09 17:29:44 +0530

    Final

--- Aditya_c/src/test/java/runner/RunnerClass.java
+++ Aditya_c/src/test/java/runner/RunnerClass.java
@@ -3,9 +3,9 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
-		features = "featurefiles/Login.feature",
+		features = "FeatureFiles/CampaignsModule.feature",
 		glue={"stepdefinitions"},
-		dryRun = true,
+		dryRun = false,
 		monochrome = true,
 		plugin={"html:target/report.html","json:target/report.json"}
 		)
