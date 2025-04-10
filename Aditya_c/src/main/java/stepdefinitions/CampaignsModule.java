79e1fad437e1230194988e2d96424cf7974c3471
admin <admin@DESKTOP-4RM8KQM>
2025-04-10 15:14:00 +0530

    a

--- Aditya_c/src/main/java/stepdefinitions/CampaignsModule.java
+++ Aditya_c/src/main/java/stepdefinitions/CampaignsModule.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
@@ -23,7 +24,7 @@ public class CampaignsModule
 	@Given("User launches the browser")
 	public void user_launches_the_browser() 
 	{
-		driver = new EdgeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
 
